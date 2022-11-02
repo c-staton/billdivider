bfcbcf9aa0f96f886c059eae74fec3f368586c85
Chris Staton <topherstaton@gmail.com>
2022-11-02 14:10:50 -0700

    fix import bug

--- src/Routes-nav/MobileNavBar.js
+++ src/Routes-nav/MobileNavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import "./MobileNavBar.css";
 import menuIcon from "../images/hamburger.png";
 import { Link, NavLink } from "react-router-dom";
